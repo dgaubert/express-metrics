4332a6a33e690d2a7d1b9a31b28584f81dfb13b6
dgaubert <danielgarciaaubert@gmail.com>
2015-03-07 18:37:57 +0100

    Feature #5, improved reporting. Furthermore, many successful refactors done

--- index.js
+++ index.js
@@ -1,76 +1,26 @@
-var Metrics = require('metrics');
-var report = new Metrics.Report();
+var chrono = require('./lib/chrono');
+var metrics = require('./lib/metrics');
+var header = require('./lib/header');
 
-function metrics(options) {
+function expressMetrics(options) {
   options = (typeof options === 'undefined') ? {} : options;
-
-  var CATEGORIES = {
-    all: 'all',
-    static: 'static', // i.e. "/favicon.ico"
-    status: 'status' // i.e. "status_200"
-  };
-
-  function timer(start) {
-    if (!start) {
-      return process.hrtime();
-    }
-
-    var diff = process.hrtime(start);
-    var nanoseconds = diff[0] * 1e9 + diff[1];
-
-    return options.decimals ?
-      Math.round(nanoseconds / 1e3) / 1e3 : // time in ms with 3 decimals
-      Math.round(nanoseconds / 1e6); // rounded time to ms
-  }
-
-  function updateMetric(name, time) {
-    if (!report.getMetric(name)) {
-      report.addMetric(name, new Metrics.Timer());
-    }
-
-    report.getMetric(name).update(time);
-  }
-
-  function getMetricName(route, methodName) {
-    var routeName = CATEGORIES.static;
-
-    if (route && route.path) {
-      routeName = route.path;
-
-      if (Object.prototype.toString.call(routeName) === '[object RegExp]') {
-        routeName = routeName.source;
-      }
-
-      routeName = methodName + '_' + routeName;
-    }
-
-    return routeName;
-  }
-
-  function updateMetrics(req, res, responseTime) {
-    var metricName = getMetricName(req.route, req.method);
-
-    updateMetric(CATEGORIES.all, responseTime);
-    updateMetric(CATEGORIES.status + '_' + res.statusCode, responseTime);
-    updateMetric(metricName, responseTime);
-  }
+  chrono.init({ decimals: options.decimals });
+  header.init({ header: options.header} );
 
   return function (req, res, next) {
-    var startAt = timer();
+    chrono.start();
 
     // decorate response#end method from express
     var end = res.end;
     res.end = function () {
-      var responseTime = timer(startAt);
+      var responseTime = chrono.stop();
 
-      if (options.header) {
-        res.setHeader('X-Response-Time', responseTime + 'ms');
-      }
+      header.setResponseTime(res, responseTime);
 
       // call to original express#res.end()
       end.apply(res, arguments);
 
-      updateMetrics(req, res, responseTime);
+      metrics.update(req, res.statusCode, responseTime);
     };
 
     next();
@@ -78,14 +28,13 @@ function metrics(options) {
 }
 
 function getSummary() {
-  // avoid wrapped object
-  return report.summary()[''];
+  return metrics.summary();
 }
 
 function jsonSummary(req, res) {
   res.json(getSummary());
 }
 
-module.exports = metrics;
+module.exports = expressMetrics;
 module.exports.getSummary = getSummary;
 module.exports.jsonSummary = jsonSummary;
---
+++ test/header.spec.js
@@ -0,0 +1,37 @@
+var header = require('../lib/header');
+
+describe('Test suite for header module', function () {
+  it('module should be loaded properly', function () {
+    header.should.Object;
+    header.init.should.Function;
+    header.setResponseTime.should.Function;
+  });
+
+  describe('checking options behavior', function () {
+
+    it('when init is not called should do nothing', function () {
+      var res = {};
+      header.setResponseTime(res, 0);
+    });
+
+    it('when header is not specified should do nothing', function () {
+      var res = {};
+      header.init();
+      header.setResponseTime(res, 0);
+    });
+
+    it('when header is set to false, should do nothing', function () {
+      var res = {};
+      header.init({ header: false });
+      header.setResponseTime(res, 0);
+    });
+
+    it('when header is set to true, should do set X-Response-Time header', function () {
+      var res = {};
+      header.init({ decimals: true });
+      header.setResponseTime(res, 0);
+      res.writeHead.should.Function;
+    });
+
+  });
+});
