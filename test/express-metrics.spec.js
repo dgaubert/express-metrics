7fd951143256a0db1ee168582ba9a18fe81f491a
Daniel Garcia Aubert <daniel.garcia@taptapnetworks.com>
2015-03-12 10:35:53 +0100

    Improving tests

--- test/express-metrics.spec.js
+++ test/express-metrics.spec.js
@@ -2,34 +2,40 @@ var request = require('supertest');
 var express = require('express');
 var metrics = require('../index');
 
-describe('Test suit for express-metrics middleware', function(){
+describe('Test suit for express-metrics middleware', function () {
   var app = express();
 
   app.use(metrics());
 
-  app.get('/', function (req, res){
+  app.get('/', function (req, res) {
     res.json({});
   });
-
-
-  before(function (done) {
-    request(app).get('/')
-      .end(function (err) {
-        if (err) {
-          return done(err);
-        }
-        done();
-      });
-  });
-
-  it('should return a json with default metrics', function () {
-    var summary = metrics.getSummary();
-
-    summary.should.have.property('global');
-    summary.global.should.have.property('all');
-    summary.global.all.should.have.property('type');
-    summary.should.have.property('/');
-    summary['/'].get.should.have.property('type');
-  });
   
+  describe('when makes a request to root path at first time', function () {
+
+    before(function (done) {
+      request(app).get('/')
+        .end(function (err) {
+          if (err) {
+            return done(err);
+          }
+          done();
+        });
+    });
+
+    it('.summary should return an object with default metrics', function () {
+      var summary = metrics.getSummary();
+
+      summary.should.Object;
+      summary.should.have.property('global');
+      summary.global.should.have.property('all');
+      summary.should.have.property('/');
+      summary.should.have.property('method');
+      summary.method.should.have.property('get');
+      summary.should.have.property('status');
+      summary.status.should.have.property('200');
+    });
+
+  });
+
 });
